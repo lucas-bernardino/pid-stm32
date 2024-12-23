24075c6755a89472e01fc26e94f9fac07752ff5b
Lucas Gabriel Bernardino <122226174+lucas-bernardino@users.noreply.github.com>
2024-12-23 12:09:00 -0300

    Delete Debug directory

--- Debug/Src/VL53L0X.d
+++
@@ -1,5 +0,0 @@
-Src/VL53L0X.o: ../Src/VL53L0X.c \
- C:/Users/Windows\ 10/Documents/GitHub/Kyros/str-miros-stm32f103-master/Inc/VL53L0X.h \
- C:/Users/Windows\ 10/Documents/GitHub/Kyros/str-miros-stm32f103-master/Inc/stm32.h
-C:/Users/Windows\ 10/Documents/GitHub/Kyros/str-miros-stm32f103-master/Inc/VL53L0X.h:
-C:/Users/Windows\ 10/Documents/GitHub/Kyros/str-miros-stm32f103-master/Inc/stm32.h:
--- Debug/Src/stm32.d
+++
@@ -1,3 +0,0 @@
-Src/stm32.o: ../Src/stm32.c \
- C:/Users/Windows\ 10/Documents/GitHub/Kyros/str-miros-stm32f103-master/Inc/stm32.h
-C:/Users/Windows\ 10/Documents/GitHub/Kyros/str-miros-stm32f103-master/Inc/stm32.h:
